f6b7d1a0cbb7efe15351dbbf45e11596934da20e
tatyana-best <tatyana.best@list.ru>
2022-10-02 12:31:33 +0300

    Addition iblock activity

--- local/activities/custom/ibgetdataactivity/.description.php
+++ local/activities/custom/ibgetdataactivity/.description.php
@@ -10,33 +10,6 @@ $arActivityDescription = array(
 	'CATEGORY' => array(
 		'ID' => 'other',
 	),
-	//'ADDITIONAL_RESULT' => ['IblockFieldsResult'],	
-
-	'RETURN' => array(
-		'IblockElementId' => array(
-			'NAME' => 'ID найденного элемента',
-			'TYPE' => 'int',
-		),
-		/*'CountFailDeals' => array(
-			'NAME' => GetMessage('DEALS_INVOICE_CONTACT_ACTIVITY_COUNT_FAIL_DEALS'),
-			'TYPE' => 'int',
-		),
-		'CountSuccessDeals' => array(
-			'NAME' => GetMessage('DEALS_INVOICE_CONTACT_ACTIVITY_COUNT_SUCCESS_DEALS'),
-			'TYPE' => 'int',
-		),
-		'SumSuccessDeals' => array(
-			'NAME' => GetMessage('DEALS_INVOICE_CONTACT_ACTIVITY_SUM_SUCCESS_DEALS'),
-			'TYPE' => 'int',
-		),
-		'SumFailInoice' => array(
-			'NAME' => GetMessage('DEALS_INVOICE_CONTACT_ACTIVITY_SUM_FAIL_INVOICE'),
-			'TYPE' => 'int',
-		),
-		'SumSuccessInoice' => array(
-			'NAME' => GetMessage('DEALS_INVOICE_CONTACT_ACTIVITY_SUM_SUCCESS_INVOICE'),
-			'TYPE' => 'int',
-		),	*/
-	),
+	'ADDITIONAL_RESULT' => ['IblockFieldsResult'],	
 	
 );
